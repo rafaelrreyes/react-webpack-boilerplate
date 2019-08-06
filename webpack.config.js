176b965818fbc6e437109181735f37c70be3ae44
rafael <rafaelrreyes@yahoo.com>
2019-08-05 19:32:26 -0700

    Updating webpack/babel and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    },
-    devServer: {
+	entry: "./src/index.js",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+						plugins: ["@babel/plugin-proposal-class-properties"]
+					}
+				}
+			}
+		]
+	},
+	devServer: {
 		port: 3000,
 		proxy: {
-			'/api/**': {
-				target: 'http://localhost:6200'	
+			"/api/**": {
+				target: "http://localhost:6200"
 			}
 		}
 	},
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        })
-    ]
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html"
+		})
+	]
 };
